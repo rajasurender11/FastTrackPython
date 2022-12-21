dce684185aa65bff039ede091047c042133eafd0
rajasurender11 <rajasurender11@gmail.com>
2022-12-21 14:37:05 +0530

    First commit

---
+++ com/demo/FirstPython.py
@@ -0,0 +1,59 @@
+print("HI")
+"""
+print("welcome to python class")
+print('This is python first class')
+print("I'm fine , i can't ")
+
+"""
+
+print("Welcome")
+#define  datatypes # number --> integer(int) #float # string # bool
+id = 10
+a= 20
+b =49
+c =0
+n1 =90
+sal = 45.6
+emp_name = "surender"
+
+f = True
+f1 = False
+
+print(id)
+print(f"value of id is {id}")
+print(a)
+print(sal)
+print(emp_name)
+print(f"value of  a {a} and value of sal {sal}")
+
+print(f1)
+
+a= 10
+b = 20
+c = a+b
+print(f"value of c is {c}")
+print(c)
+s1 = 21
+s2 = "10"  # 21 + "10"  ==> 21 + 10
+s3 = s1 +int(s2)
+print(s3)
+
+s4 = "56"
+s_result = float(s1+int(s4))
+print(s_result)
+
+f4 = 11.2
+print(int(f4))
+
+m1 = 10
+m2 = 2
+m3 = (m1*m2)/3  #(10*2)/2 => 20/2  ==> 10
+print(m3)
+
+n1 =90
+n2 =24
+n3 = n1-n2
+print(n3)
+
+r = int(((10*2+7+9)/8)*4.4)
+print(r)
